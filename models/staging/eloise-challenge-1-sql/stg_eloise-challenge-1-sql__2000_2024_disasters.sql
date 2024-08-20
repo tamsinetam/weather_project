be3cc07e17d144cc62bb3a4e6ac2267cc18ba066
elo c <eloise.cail@hotmail.fr>
2024-08-20 08:44:33 +0000

    create table

---
+++ models/staging/eloise-challenge-1-sql/stg_eloise-challenge-1-sql__2000_2024_disasters.sql
@@ -0,0 +1,51 @@
+with 
+
+source as (
+
+    select * from {{ source('eloise-challenge-1-sql', '2000_2024_disasters') }}
+
+),
+
+renamed as (
+
+    select
+        disno_,
+        classification_key,
+        disaster_subgroup,
+        disaster_type,
+        disaster_subtype,
+        event_name,
+        iso,
+        country,
+        subregion,
+        region,
+        location,
+        origin,
+        associated_types,
+        appeal,
+        declaration,
+        magnitude,
+        magnitude_scale,
+        latitude,
+        longitude,
+        start_year,
+        start_month,
+        start_day,
+        end_year,
+        end_month,
+        end_day,
+        total_deaths,
+        no__injured,
+        no__affected,
+        no__homeless,
+        total_affected,
+        total_damage___000_us__,
+        total_damage__adjusted___000_us__,
+        cpi,
+        admin_units
+
+    from source
+
+)
+
+select * from renamed
