cec835156b397b0f8bb2b8efb5b683a035878a7f
Chloé Lemire <chl.lemire@gmail.com>
2024-08-21 09:25:15 +0000

    update explo_sam

--- models/mart/explo_sam.sql
+++ models/mart/explo_sam.sql
@@ -1,1 +1,30 @@
-select * from {{ ref('clean_wheather_disasters') }}
+SELECT 
+  disno_,
+  disaster_subgroup,
+  disaster_type,
+  disaster_subtype,
+  country,
+  magnitude,
+  magnitude_scale,
+  start_year,
+  start_month,
+  start_day,
+  end_year,
+  end_month,
+  end_day,
+  total_deaths,
+  no__injured,
+  no__affected,
+  no__homeless,
+  total_affected,
+  updated_event_name,
+  city,
+  CASE
+    -- If month is missing, return NULL
+    WHEN start_month IS NULL THEN NULL
+    -- If day is missing, use the first day of the month
+    WHEN start_day IS NULL THEN DATE(start_year, start_month, 1)
+    -- If neither is missing, construct the date from year, month, and day
+    ELSE DATE(start_year, start_month, start_day)
+  END AS start_date_gran_month
+FROM {{ ref('clean_wheather_disasters') }}
