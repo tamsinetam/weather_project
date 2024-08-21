38635b89a36b8dc840d231b302cc100377d4df75
Itole Dogmo <itole.dogmo@proton.me>
2024-08-21 09:45:06 +0000

    creation of remaining tables by hazard subtype (geophysical, meteorological) + renaming of the base table as "globale (I removed all the null values on death counts and injured counts)

---
+++ models/mart/global_deaths_injured_wheather_disasters.sql
@@ -0,0 +1,12 @@
+SELECT
+disaster_subgroup
+,disaster_type
+,disaster_subtype
+,region
+,country
+,start_year
+,start_month
+,total_deaths AS total_dead
+,no__injured AS total_injured
+FROM {{ ref('clean_wheather_disasters') }}
+WHERE total_deaths is not null AND no__injured is not null
--- models/mart/regions_hyrdological_wheather_disasters.sql
+++ models/mart/regions_hyrdological_wheather_disasters.sql
@@ -3,12 +3,12 @@ region
 ,country
 ,start_month
 ,start_year
-,updated_event_name
+---updated_event_name
 ,disaster_type
 ,disaster_subtype 
-,SUM(total_deaths) AS total_dead
-,SUM(no__injured) AS total_injured
-FROM {{ ref('regions_hyrdological_wheather_disasters') }}
-GROUP BY start_month,start_year, region, country, updated_event_name, disaster_type, disaster_subtype 
+,SUM(total_dead) AS total_dead
+,SUM(total_injured) AS total_injured
+FROM {{ ref('global_deaths_injured_wheather_disasters') }}
+GROUP BY start_month,start_year, region, country, disaster_type, disaster_subtype 
 --I exclude the null values as they add nothing on the map chart I'm building on Looker Studio
 HAVING total_dead is not null AND total_injured is not null
