6d59c4a6ab4d2598717bd9bdfac5ecded2defd89
Chloé Lemire <chl.lemire@gmail.com>
2024-08-23 09:08:34 +0000

    clean_explo_sam

--- models/mart/explo_sam.sql
+++ models/mart/explo_sam.sql
@@ -10,17 +10,8 @@ SELECT
   wd.start_year,
   wd.start_month,
   wd.start_day,
-  wd.end_year,
-  wd.end_month,
-  wd.end_day,
-  wd.total_deaths,
-  wd.no__injured,
-  wd.no__affected,
-  wd.no__homeless,
-  wd.total_affected,
-  wd.updated_event_name,
   wd.city,
-  m.magnitude_uniformized,
+  m.standardized_magnitude,
   CASE
     -- If month is missing, return NULL
     WHEN start_month IS NULL THEN NULL
@@ -30,5 +21,5 @@ SELECT
     ELSE DATE(start_year, start_month, start_day)
   END AS start_date_gran_month
 FROM {{ ref('clean_wheather_disasters') }} AS wd
-LEFT JOIN {{ ref('stg_eloise-challenge-1-sql__magnitude') }} AS m
+LEFT JOIN {{ ref('stg_eloise-challenge-1-sql__magnitude_final') }} AS m
 USING (disno_)
