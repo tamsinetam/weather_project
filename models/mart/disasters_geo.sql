3ccadb0d5854a60f2846b35925a2ae9e77349f01
Louise Beros <louise.beros.beros@gmail.com>
2024-08-23 09:25:29 +0000

    add magnitude final to disasters_geo

--- models/mart/disasters_geo.sql
+++ models/mart/disasters_geo.sql
@@ -9,5 +9,8 @@ updated_event_name
 ,subregion
 ,region
 ,city
+,disno_
 FROM {{ ref('clean_wheather_disasters') }}
+LEFT JOIN {{ ref('stg_eloise-challenge-1-sql__magnitude_final') }} AS m
+USING (disno_)
 
