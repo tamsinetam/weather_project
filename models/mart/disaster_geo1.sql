43d2fc7461cb0b84f9b28ed53ace99570a86bef9
Louise Beros <louise.beros.beros@gmail.com>
2024-08-23 13:19:36 +0000

    final_table_geo_

--- models/mart/disaster_geo1.sql
+++ models/mart/disaster_geo1.sql
@@ -1,13 +1,18 @@
 SELECT
-disno_
-,updated_event_name
-,disaster_subgroup
-,disaster_type
-,disaster_subtype
-,associated_types
-,iso
-,subregion
-,region
-,standardized_magnitude
-FROM {{ ref('geo_magnitude') }}
-LEFT JOIN {{ ref('stg_eloise-challenge-1-sql__coordinates_cities_countries') }} USING (disno_)
+b.disno_
+,b.updated_event_name
+,b.disaster_subgroup
+,b.disaster_type
+,b.disaster_subtype
+,b.associated_types
+,b.iso
+,b.subregion
+,b.region
+,b.country
+,m.city
+,b.standardized_magnitude
+,m.latitude
+,m.longitude
+FROM {{ ref('geo_magnitude') }} AS b 
+LEFT JOIN {{ ref('stg_eloise-challenge-1-sql__coordinates_cities_countries') }} AS m 
+USING (disno_)
