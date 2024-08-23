ed411270ebeff8fce66dd68576795823799a53d3
Louise Beros <louise.beros.beros@gmail.com>
2024-08-23 10:19:38 +0000

    add a table geo and magnitutide

---
+++ models/mart/geo_magnitude.sql
@@ -0,0 +1,17 @@
+SELECT 
+d.updated_event_name
+,d.disaster_subgroup
+,d.disaster_type
+,d.disaster_subtype
+,d.associated_types
+,d.iso
+,d.country
+,d.subregion
+,d.region
+,d.city
+,d.disno_
+,m.standardized_magnitude
+FROM {{ ref('clean_wheather_disasters') }} AS d 
+LEFT JOIN {{ ref('stg_eloise-challenge-1-sql__magnitude_final') }} AS  m
+USING (disno_)
+
